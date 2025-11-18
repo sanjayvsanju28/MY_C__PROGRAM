7d3797d7bcc344729d5bb7eedb8dec44e4ef25bb
sanjayvsanju28 <sanjayvsanju28@gmail.com>
2025-11-18 18:23:57 +0530

    Create largest of two number .c

---
+++ largest of two number .c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    int n1,n2;
+    printf("enter the values for n1 and n2");
+    scanf("%d %d\n",&n1,&n2);
+    if(n1>n2){
+        printf("enter n1 is largest%d",n1);
+    }
+        else{
+           printf(" enter n2 is largest %d",n2); 
+        }
+    
+    
+
+    return 0;
+}
