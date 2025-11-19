bce5ecb5cceadf8ef8fd6b4d24f263106717db25
sanjayvsanju28 <sanjayvsanju28@gmail.com>
2025-11-19 13:43:17 +0530

    Create  sum of arrays .c

---
+++ sum of arrays .c
@@ -0,0 +1,17 @@
+// sum of arrays
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+    printf("enter the number of elements");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the elements");
+    for( int i=0;i<n;i++){
+    scanf("%d",&arr[n]);
+    sum+=arr[i];
+    }
+    printf("%d\n",sum);
+}
+      
+    
+        
