07ff8e67632cd99fa14697cfa45b07f74f2986e0
sanjayvsanju28 <sanjayvsanju28@gmail.com>
2025-11-20 10:47:47 +0530

    Create  string copy .c

---
+++ string copy .c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char start[50] = "go!";
+    char end[50];
+    strcpy(start,end);
+  printf(" start :%s\n",start);
+  printf(" end :%s\n",end);
+}
